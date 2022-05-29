ba46306cbbed15e1c73a23d796a21b2e6064db10
Dmytro Tarasov <dtarasov24s@gmail.com>
2022-05-29 23:13:47 +0300

    End of section 10

---
+++ client/src/app/shared/components/pager/pager.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+    selector: 'app-pager',
+    templateUrl: './pager.component.html',
+    styleUrls: ['./pager.component.scss']
+})
+export class PagerComponent implements OnInit {
+    @Input() totalCount: number;
+    @Input() pageSize: number;
+    @Output() pageChanged = new EventEmitter<number>(); // 'Output' property to pass data 
+    // from child component to a parent
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+    onPagerChange(event: any) {
+        this.pageChanged.emit(event.page);
+    } 
+
+}
